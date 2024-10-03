24adb510046ced4c86595633c4d71f556f5e5432
pedro <pedrohsdleg@gmail.com>
2024-10-03 00:50:22 -0300

    fix modelovistoria controller

--- autopericiado/src/modelo-de-vistoria/controller/ModeloVistoria.controller.ts
+++ autopericiado/src/modelo-de-vistoria/controller/ModeloVistoria.controller.ts
@@ -1,6 +1,11 @@
 import { Controller } from '@nestjs/common';
 import { BaseController } from '../../base.controller';
 import { ModeloVistoria } from '../entity/modelo-vistoria.entity';
+import { ModeloVistoriaService } from '../service/ModeloVistoria.service';
 
 @Controller('modelo-vistoria')
-export class ModeloVistoriaController extends BaseController<ModeloVistoria> {}
+export class ModeloVistoriaController extends BaseController<ModeloVistoria> {
+  constructor(protected readonly baseService: ModeloVistoriaService) {
+    super(baseService);
+  }
+}
